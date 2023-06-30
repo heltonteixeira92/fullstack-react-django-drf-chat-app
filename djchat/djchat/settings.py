eda721f633427c6e86e3ecb188c6f8a7c3d236c8
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2023-06-30 10:31:20 -0300

    account app created and django user extended

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     # local
     'server',
+    'account',
 
     # 3rd
     'rest_framework',
@@ -130,3 +131,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'account.Account'
