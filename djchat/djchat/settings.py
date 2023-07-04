679661d98170196ff4088903e20477eb6d785943
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2023-07-04 00:55:30 -0300

    configuring DRF-Spectacular & Swagger UI

--- djchat/djchat/settings.py
+++ djchat/djchat/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
     # 3rd
     'rest_framework',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,15 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'account.Account'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
+}
