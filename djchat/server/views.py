0d9468b71b70c127adb2f5d5d5dc57dbc45cfe7e
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2023-07-11 09:58:00 -0300

    creating an API endpoint for filtering

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .serializer import ServerSerializer
+from rest_framework.response import Response
+from .models import Server
+
+
+class ServerListViewSet(viewsets.ViewSet):
+
+    queryset = Server.objects.all()
+
+    def list(self, request):
+        category = request.query_params.get("category")
+
+        if category:
+            self.queryset = self.queryset.filter(category__name=category)
+
+        serializer = ServerSerializer(self.queryset, many=True)
+
+        return Response(serializer.data)
