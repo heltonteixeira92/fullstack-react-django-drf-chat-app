2a6c507084d0cb1a62582be0bae43bfb27e5b79d
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2023-07-12 14:14:20 -0300

    server associated channels

--- djchat/server/serializer.py
+++ djchat/server/serializer.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 
-from .models import Server
+from .models import Server, Channel, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = "__all__"
+
+
+class ChannelSerialiazer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = "__all__"
 
 
 class ServerSerializer(serializers.ModelSerializer):
+    channel_server = ChannelSerialiazer(many=True)
 
     class Meta:
         model = Server
