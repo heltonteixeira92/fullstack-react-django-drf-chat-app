8d52085dd6be59d20ad07942b1faeb7e5dd09db6
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2023-07-14 11:33:40 -0300

    creating an API endpoint decorator for Detailing Endpoints

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -1,10 +1,11 @@
 from django.db.models import Count
 from rest_framework import viewsets
-from rest_framework.exceptions import ValidationError, AuthenticationFailed
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
+from rest_framework.response import Response
 
 from .serializer import ServerSerializer
-from rest_framework.response import Response
 from .models import Server
+from .schema import server_list_docs
 
 
 class ServerListViewSet(viewsets.ViewSet):
@@ -14,6 +15,7 @@ class ServerListViewSet(viewsets.ViewSet):
 
     queryset = Server.objects.all()
 
+    @server_list_docs
     def list(self, request):
         """
         Retrieve a list of servers based on the provided filters.
@@ -32,16 +34,16 @@ class ServerListViewSet(viewsets.ViewSet):
         by_serverid = request.query_params.get("by_serverid")
         with_num_members = request.query_params.get("with_num_members") == "true"
 
-        # Check if user authentication is required
-        if by_user and not request.user.is_authenticated:
-            raise AuthenticationFailed()
-
         # Filter servers by category
         if category:
             self.queryset = self.queryset.filter(category__name=category)
 
         # Filter servers by user
         if by_user:
+            # Check if user is authenticated
+            if not request.user.is_authenticated:
+                raise AuthenticationFailed()
+
             user_id = request.user.id
             self.queryset = self.queryset.filter(member=user_id)
 
@@ -51,6 +53,9 @@ class ServerListViewSet(viewsets.ViewSet):
 
         # Filter server by server ID
         if by_serverid:
+            if not request.user.is_authenticated:
+                raise AuthenticationFailed()
+
             try:
                 self.queryset = self.queryset.filter(id=by_serverid)
                 if not self.queryset.exists():
