1f1b846825a52de7fc14344cee78f06785bb968c
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2023-07-17 21:28:17 -0300

    model refactor  for icons and banners for server

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -3,6 +3,15 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
 
+
+def server_icon_upload_path(instance, filename):
+    return f"server/{instance.id}/server_icon/{filename}"
+
+
+def server_banner_upload_path(instance, filename):
+    return f"server/{instance.id}/server_banner/{filename}"
+
+
 def category_icon_upload_path(instance, filename):
     return f"category/{instance.id}/category_icon/{filename}"
 
@@ -17,7 +26,7 @@ class Category(models.Model):
             existing = get_object_or_404(Category, id=self.id)
             if existing.icon != self.icon:
                 existing.icon.delete(save=False)
-                super(Category, self).save(*args, **kwargs)
+        super(Category, self).save(*args, **kwargs)
 
     @receiver(models.signals.pre_delete, sender="server.Category")
     def category_delete_files(sender, instance, **kwargs):
@@ -47,10 +56,25 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
-    
+    banner = models.ImageField(upload_to=server_banner_upload_path, blank=True, null=True)
+    icon = models.ImageField(upload_to=server_icon_upload_path, blank=True, null=True)
+
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()
-        super(Channel, self).save()
+        if self.id:
+            existing = get_object_or_404(Category, id=self.id)
+            if existing.icon != self.icon:
+                existing.icon.delete(save=False)
+            if existing.banner != self.banner:
+                existing.banner.delete(save=False)
+        super(Channel, self).save(*args, **kwargs)
+
+    @receiver(models.signals.pre_delete, sender="server.Channel")
+    def category_delete_files(sender, instance, **kwargs):
+        for field in instance._meta.fields:
+            if field.name == "icon" or field.name == "banner":
+                file = getattr(instance, field.name)
+                if file:
+                    file.delete(save=False)
     
     def __str__(self):
         return self.name
