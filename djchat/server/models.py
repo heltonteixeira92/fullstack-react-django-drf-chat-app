ae5ab81a35a9dc21cdfb35c4a4dd651126062486
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2023-07-17 21:44:45 -0300

    django model validation class for image field

--- djchat/server/models.py
+++ djchat/server/models.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.dispatch import receiver
 
+from .validators import validate_icon_image_size, validate_image_file_extension
+
 
 def server_icon_upload_path(instance, filename):
     return f"server/{instance.id}/server_icon/{filename}"
@@ -56,8 +58,18 @@ class Channel(models.Model):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="channel_owner")
     topic = models.CharField(max_length=100)
     server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name="channel_server")
-    banner = models.ImageField(upload_to=server_banner_upload_path, blank=True, null=True)
-    icon = models.ImageField(upload_to=server_icon_upload_path, blank=True, null=True)
+    banner = models.ImageField(
+        upload_to=server_banner_upload_path,
+        blank=True,
+        null=True,
+        validators=[validate_image_file_extension]
+    )
+    icon = models.ImageField(
+        upload_to=server_icon_upload_path,
+        blank=True,
+        null=True,
+        validators=[validate_icon_image_size, validate_image_file_extension]
+    )
 
     def save(self, *args, **kwargs):
         if self.id:
@@ -68,8 +80,8 @@ class Channel(models.Model):
                 existing.banner.delete(save=False)
         super(Channel, self).save(*args, **kwargs)
 
-    @receiver(models.signals.pre_delete, sender="server.Channel")
-    def category_delete_files(sender, instance, **kwargs):
+    @receiver(models.signals.pre_delete, sender="server.Server")
+    def server_delete_files(sender, instance, **kwargs):
         for field in instance._meta.fields:
             if field.name == "icon" or field.name == "banner":
                 file = getattr(instance, field.name)
