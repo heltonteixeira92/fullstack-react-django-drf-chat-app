ce874a550abfe32e122da64233c91b957dba7844
Helton Teixeira de Souza <heltonteixeiradesouza@hotmail.com>
2023-10-26 10:19:58 -0300

    creating the primary app bar (part-2) responsive

--- reactchat/src/pages/templates/PrimaryAppBar.tsx
+++ reactchat/src/pages/templates/PrimaryAppBar.tsx
@@ -1,10 +1,35 @@
-import { AppBar, Toolbar, Link, Typography }  from '@mui/material';
+import {AppBar, Box, IconButton, Toolbar, Link, Typography, Drawer, useMediaQuery} from '@mui/material';
 import { useTheme } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
+import {useEffect, useState} from "react";
 
 const PrimaryAppBar = () => {
+    const [sideMenu, setSideMenu] = useState(false);
     const theme = useTheme();
+
+    const isSmallScreen = useMediaQuery(theme.breakpoints.up("sm"));
+
+    useEffect(() => {
+        if (isSmallScreen && sideMenu) {
+            setSideMenu(false);
+        }
+    })
+
+    const toggleDrawer =
+        (open: Boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+            event.type == "keydown" &&
+            ((event as React.KeyboardEvent).key == "Tab" ||
+                (event as React.KeyboardEvent).key == "Shift")
+        ) {
+            return;
+        }
+            setSideMenu(open);
+        };
+
     return (
         <AppBar sx={{
+            zIndex: (theme) => theme.zIndex.drawer + 2,
             backgroundColor: theme.palette.background.default,
             borderBottom: `1px solid ${theme.palette.divider}`,
         }}>
@@ -12,6 +37,25 @@ const PrimaryAppBar = () => {
                 height: theme.primaryAppBar.height,
                 minHeight: theme.primaryAppBar.height,
             }}>
+                <Box sx={{display: {xs: "block", sm: "none"}}}>
+                    <IconButton
+                        color="inherit"
+                        arial-label="open drawer"
+                        edge="start"
+                        onClick={toggleDrawer(true)}
+                        sx={{mr:2}}>
+                        <MenuIcon/>
+                    </IconButton>
+
+                </Box>
+
+                <Drawer anchor="left" open={sideMenu} onClose={toggleDrawer(false)}>
+                {[...Array(100)].map((_,i) => (
+                    <Typography key={i} paragraph>
+                        {i + 1}
+                    </Typography>
+                ))}
+                </Drawer>
                 <Link href="/" underline="none" color="inherit">
                     <Typography
                         variant="h6"
